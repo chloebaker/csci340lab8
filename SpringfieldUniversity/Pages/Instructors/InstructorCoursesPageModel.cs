b29f265a40d77f4ec68e04d1c131d45d04595970
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-10-29 11:40:38 -0500

    updated instructor edit page

---
+++ SpringfieldUniversity/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,32 @@
+﻿using SpringfieldUniversity.Data;
+using SpringfieldUniversity.Models;
+using SpringfieldUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpringfieldUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context,
+                                               Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
