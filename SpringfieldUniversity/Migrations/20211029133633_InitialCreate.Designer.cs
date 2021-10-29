b52b246cca56b46da107d50c41387ffefbc8fec6
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-10-29 08:52:51 -0500

    updated database

--- SpringfieldUniversity/Migrations/20211029133633_InitialCreate.Designer.cs
+++ SpringfieldUniversity/Migrations/20211029133633_InitialCreate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpringfieldUniversity.Data;
 
 namespace SpringfieldUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20211029133633_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
